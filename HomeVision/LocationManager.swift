1cbfe7e8457711e60595b1cf8758b445d0804ea0
Batuhan Demirbaş <batuhan@demirbas.co>
2023-03-30 21:11:52 +0300

    ventilation slider and animation edited

---
+++ HomeVision/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  HomeVision
+//
+//  Created by Batuhan on 29.03.2023.
+//
+
+import Foundation
---
+++ HomeVision/View/Slider.swift
@@ -0,0 +1,20 @@
+//
+//  Slider.swift
+//  HomeVision
+//
+//  Created by Batuhan on 30.03.2023.
+//
+
+import UIKit
+
+class Slider: UISlider {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
